a2c6e8e265213918f1022393b0964711b1484420
nalinsharma9 <nalin.sharma1998@gmail.com>
2024-06-10 16:34:29 +0530

    Modifying controller for REST webservices

--- bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
+++ bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
@@ -2,9 +2,11 @@ package com.telusko.demo.bootjpa.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.demo.bootjpa.dao.AlienRepo;
@@ -29,16 +31,28 @@ public class AlienController {
         return "home.jsp";
     }
 
-    @RequestMapping("/getAlien")
-    public ModelAndView getAlien(@RequestParam int aid){
-        ModelAndView mv = new ModelAndView("showAlien.jsp");
-        Alien alien = repo.findById(aid).orElse(new Alien());
-        mv.addObject(alien);
+//    @RequestMapping("/getAlien")
+//    public ModelAndView getAlien(@RequestParam int aid){
+//        ModelAndView mv = new ModelAndView("showAlien.jsp");
+//        Alien alien = repo.findById(aid).orElse(new Alien());
+//        mv.addObject(alien);
+//
+//        System.out.println(repo.findByTech("Java"));
+//        System.out.println(repo.findByAidGreaterThan(101));
+//        System.out.println(repo.findByTechSorted("Java"));
+//        return mv;
+//    }
 
-        //System.out.println(repo.findByTech("Java"));
-        //System.out.println(repo.findByAidGreaterThan(101));
-        System.out.println(repo.findByTechSorted("Java"));
-        return mv;
+    @RequestMapping("aliens")
+    @ResponseBody //this annotation will return the data as it is without looking for any view name
+    public String getAliens(){
+        return repo.findAll().toString();
+    }
+
+    @RequestMapping("alien/{aid}")
+    @ResponseBody //this annotation will return the data as it is without looking for any view name
+    public String getAliens(@PathVariable("aid") int aid){
+        return repo.findById(aid).toString();
     }
 
 
--- bootjpa/src/main/java/com/telusko/demo/bootjpa/dao/AlienRepo.java
+++ bootjpa/src/main/java/com/telusko/demo/bootjpa/dao/AlienRepo.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface AlienRepo extends CrudRepository<Alien , Integer>{
     //jpa automatically creates the query for us if method name starts with "findBy or getBy" and
     //method name should end with the field name in the entity class
-    List<Alien> findByTech(String tech);
-    List<Alien> findByAidGreaterThan(int aid);
+    //List<Alien> findByTech(String tech);
+//    List<Alien> findByAidGreaterThan(int aid);
 
     //we can create custom queries using @Query annotation and JPQL
-    @Query("from Alien where tech=?1 order by aname")
-    List<Alien> findByTechSorted(String tech);
+//    @Query("from Alien where tech=?1 order by aname")
+//    List<Alien> findByTechSorted(String tech);
 }
