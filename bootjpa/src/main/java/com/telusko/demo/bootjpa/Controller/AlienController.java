4bb95286853caaadd4ea31cb9cc3370883d9dc87
nalinsharma9 <nalin.sharma1998@gmail.com>
2024-06-10 13:54:36 +0530

    JPA query methods

--- bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
+++ bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
@@ -34,7 +34,10 @@ public class AlienController {
         ModelAndView mv = new ModelAndView("showAlien.jsp");
         Alien alien = repo.findById(aid).orElse(new Alien());
         mv.addObject(alien);
-        
+
+        //System.out.println(repo.findByTech("Java"));
+        //System.out.println(repo.findByAidGreaterThan(101));
+        System.out.println(repo.findByTechSorted("Java"));
         return mv;
     }
 
--- bootjpa/src/main/java/com/telusko/demo/bootjpa/dao/AlienRepo.java
+++ bootjpa/src/main/java/com/telusko/demo/bootjpa/dao/AlienRepo.java
@@ -1,10 +1,19 @@
 package com.telusko.demo.bootjpa.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.telusko.demo.bootjpa.model.Alien;
 
+import java.util.List;
+
 public interface AlienRepo extends CrudRepository<Alien , Integer>{
+    //jpa automatically creates the query for us if method name starts with "findBy or getBy" and
+    //method name should end with the field name in the entity class
+    List<Alien> findByTech(String tech);
+    List<Alien> findByAidGreaterThan(int aid);
 
-
+    //we can create custom queries using @Query annotation and JPQL
+    @Query("from Alien where tech=?1 order by aname")
+    List<Alien> findByTechSorted(String tech);
 }
