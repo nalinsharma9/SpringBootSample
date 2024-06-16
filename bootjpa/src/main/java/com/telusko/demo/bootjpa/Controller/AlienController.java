8bf7cd1c915445c48a8526cda028c699c7260fee
nalinsharma9 <nalin.sharma1998@gmail.com>
2024-06-16 12:42:26 +0530

    Using REST controller and REST mapping annotations for Post,Get

--- bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
+++ bootjpa/src/main/java/com/telusko/demo/bootjpa/Controller/AlienController.java
@@ -2,11 +2,16 @@ package com.telusko.demo.bootjpa.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.demo.bootjpa.dao.AlienRepo;
@@ -16,7 +21,10 @@ import java.util.List;
 import java.util.Optional;
 
 
-@Controller
+//If we are building a rest application, we use @RestController annotation and
+// comment @ResponseBody so that the controller doesn't return the view name and returns serialized data
+//@Controller
+@RestController
 public class AlienController {
 
     @Autowired
@@ -28,11 +36,11 @@ public class AlienController {
     }
 
 
-    @RequestMapping("/addAlien")
-    public String addAlien(Alien alien){
-        repo.save(alien);
-        return "home.jsp";
-    }
+//    @RequestMapping("/addAlien")
+//    public String addAlien(Alien alien){
+//        repo.save(alien);
+//        return "home.jsp";
+//    }
 
 //    @RequestMapping("/getAlien")
 //    public ModelAndView getAlien(@RequestParam int aid){
@@ -46,17 +54,25 @@ public class AlienController {
 //        return mv;
 //    }
 
+    @PostMapping("/alien")
+    //@RequestBody annotation will convert the incoming json data into java object by deserialization
+    public Alien addAlien(@RequestBody Alien alien){
+        repo.save(alien);
+        return alien;
+    }
+
+
     //produces will restrict the data to be returned only in the format mentioned
-    //@RequestMapping(value = "aliens",produces = {"application/xml"})
-    @RequestMapping(value = "aliens")
-    @ResponseBody //this annotation will return the data as it is without looking for any view name
+    //@GetMapping(value = "aliens",produces = {"application/xml"})
+    @GetMapping(value = "aliens")
+//    @ResponseBody //this annotation will return the data as it is without looking for any view name
     public List<Alien> getAliens(){
         //jackson-core automatically converts java object into Json when we return data from controller
         return repo.findAll();
     }
 
-    @RequestMapping("alien/{aid}")
-    @ResponseBody //this annotation will return the data as it is without looking for any view name
+    @GetMapping("alien/{aid}")
+//    @ResponseBody //this annotation will return the data as it is without looking for any view name
     public Optional<Alien> getAliens(@PathVariable("aid") int aid){
         return repo.findById(aid);
     }
